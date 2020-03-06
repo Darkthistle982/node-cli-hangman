e14cb50b7f8620203889d21c2adf12078771ee07
Brian Allen <brianallen2401@gmail.com>
2020-03-06 13:24:03 -0700

    added words to choose from for the game, and basic variables to hold data as the game is played

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let Word = require('./word.js');
 let inquirer = require('inquirer');
 
-const wordListing = [""]
+const wordListing = ["STRENGTH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "WISDOM", "CHARISMA", "BARBARIAN", "BARD", "CLERIC", "DRUID",
+ "FIGHTER", "MONK", "PALADIN", "RANGER", "ROGUE", "SORCERER", "WARLOCK", "WIZARD"];
+
+ let selectWord = 0;
+ let chosenWord = "";
+ let gameWord = "";
+ let counter = 0;
+
+ function runGame() {
+     if (wordListing.length < 1)
+ }
