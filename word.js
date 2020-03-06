0123c073a0d078dcdf5a1f2f5537cc0053aceb6b
Brian Allen <brianallen2401@gmail.com>
2020-03-06 13:04:20 -0700

    added word constructor and related functions as well as export

---
+++ word.js
@@ -0,0 +1,29 @@
+let Letter = require('./letter.js');
+
+function Word(wordArray) {
+    this.wordArray = wordArray;
+    this.letterArray = [];
+}
+
+Word.prototype = {
+    createWord: function () {
+        for (let i = 0; i < this.wordArray.length; i++) {
+            let lettersToGuess = new Letter(this.wordArray[i]);
+            this.letterArray.push(lettersToGuess);
+        }
+    },
+    displayWord: function () {
+        let wordDisplay = [];
+        for (let i = 0; i < this.letterArray.length; i++) {
+            wordDisplay.push(this.letterArray[i].guessDisplay());
+        }
+        return wordDisplay.join(" ");
+    },
+    checkGuess: function (guess) {
+        for (let i = 0; i < this.letterArray.length; i++) {
+            this.letterArray[i].userGuessCheck(guess);
+        }
+    }
+}
+
+module.exports = Word;
