fd707f0728d3732748927edd0af15bc6392db06e
Brian Allen <brianallen2401@gmail.com>
2020-03-06 12:28:22 -0700

    added the main letter constructor

---
+++ letter.js
@@ -0,0 +1,20 @@
+function Letter(letterGuessed) {
+    this.letterGuessed = letterGuessed;
+    this.isGuessed = false;
+}
+
+Letter.prototype = {
+    guessDisplay: function () {
+        if (!this.letterGuessed) {
+            return "_";
+        } else {
+            return this.letterGuessed;
+        }
+    },
+    userGuessCheck: function (userGuess) {
+        if (userGuess === this.letterGuessed) {
+            this.isGuessed = true;
+        }
+    }
+}
+
