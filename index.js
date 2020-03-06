3e2febd316678c5336ef069b6f02142b6535a3ea
Brian Allen <brianallen2401@gmail.com>
2020-03-06 13:35:54 -0700

    wrote out function to run the main game

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ let Word = require('./word.js');
 let inquirer = require('inquirer');
 
 const wordListing = ["STRENGTH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "WISDOM", "CHARISMA", "BARBARIAN", "BARD", "CLERIC", "DRUID",
- "FIGHTER", "MONK", "PALADIN", "RANGER", "ROGUE", "SORCERER", "WARLOCK", "WIZARD"];
+    "FIGHTER", "MONK", "PALADIN", "RANGER", "ROGUE", "SORCERER", "WARLOCK", "WIZARD"];
 
- let selectWord = 0;
- let chosenWord = "";
- let gameWord = "";
- let counter = 0;
+let selectWord = 0;
+let chosenWord = "";
+let gameWord = "";
+let counter = 0;
 
- function runGame() {
-     if (wordListing.length < 1)
- }
+function runGame() {
+    if (wordListing.length < 2) {
+        wordListing = ["STRENGTH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "WISDOM", "CHARISMA", "BARBARIAN", "BARD", "CLERIC", "DRUID",
+         "FIGHTER", "MONK", "PALADIN", "RANGER", "ROGUE", "SORCERER", "WARLOCK", "WIZARD"];
+    }
+    selectWord = Math.floor(Math.random() * wordListing.length);
+    chosenWord = wordListing[selectWord];
+    gameWord = new Word(chosenWord);
+    gameWord.createWord();
+    if (selectWord > -1) {
+        wordListing.splice(selectWord, 1);
+    }
+    console.log("You have 12 guesses to find the D&D themed word!");
+    askUserInput();
+};
+
+
